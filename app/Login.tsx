838a7b447372c55de21c71ee355a3adfb4ae6ad6
Veiga <gabrielvalmeidaa@gmail.com>
2023-05-14 15:42:03 -0300

    Adiciona TypeScript no componente Login

---
+++ app/Login.tsx
@@ -0,0 +1,114 @@
+import { View, TextInput, Text } from 'react-native'
+import React, { useState } from 'react'
+import { Link, Redirect } from 'expo-router'
+import styles from './Styles'
+// import { Activity, Airplay } from '@tamagui/lucide-icons'
+import { Button, H1, Input, SizeTokens, TextArea, XStack, YStack } from 'tamagui'
+import { LinearGradient } from 'expo-linear-gradient';
+import  { getUser } from '../src/services/fetchDummyAPI'
+
+import { useFonts, LilitaOne_400Regular } from 'expo-font';
+import { AppLoading } from 'expo';
+
+import { IProductListProps } from '../src/components/ProductList'
+
+export default function Login() {
+  const [ emailInput, setEmailInput ] = useState('')
+  const [ passInput, setPassInput ] = useState('')
+  const [ validEmail, setValidEmail ] = useState(false)
+  const [apiOutput, setApiOutput ] = useState<IProductListProps[]>([])
+
+  const [fontsLoaded] = useFonts({
+    'LilitaOne-Regular': require('../assets/fonts/LilitaOne-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  validateEmail = (email) => {
+    if (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+      setValidEmail(true)
+    } else {
+      setValidEmail(false)
+    }
+  };
+
+  return (
+    <View 
+    style={{flex: 1, 
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'black',
+    }}>
+      <Text style={{
+        fontFamily: 'LilitaOne-Regular',
+        fontSize: 65,
+        color: 'rgb(255 0 128)',
+        marginBottom: 50
+        }}>Y D O O C</Text>
+
+      {/* <H1 marginBottom={'20%'} color={'$pink10Dark'} fontFamily={'LilitaOne-Regular'}>Y D O O C</H1> */}
+      {/* <View style={{width: '100%', backgroundColor: 'white', padding: 50}}> */}
+        <TextInput
+          style={styles.textInput}
+          onChangeText={ (target) => {
+            setEmailInput(target)
+            validateEmail(target)
+          } }
+          value={ emailInput }
+          placeholderTextColor="white"
+          placeholder='Email'
+          keyboardType='email-address'
+          accessibilityLabel='Digite o seu email'
+        />
+
+        <TextInput 
+          style={styles.textInput}
+          onChangeText={ (target) => setPassInput(target) }
+          value={ passInput }
+          placeholderTextColor="white"
+          placeholder='Senha'
+          secureTextEntry={ true }
+          accessibilityLabel='Digite a sua senha'
+        />
+      {/* </View> */}
+    <Link href="/products" asChild>
+      <Button
+        accessibilityLabel='Botão para fazer o login'
+        backgroundColor={['black']}
+        color={'white'}
+        borderColor='$pink10Dark'
+        borderRadius={8}
+        onPress={ () => getUser(setApiOutput)}
+        size="$6"
+        marginTop={'20%'}
+        // disabled={ (!validEmail) }
+      >
+        Login
+      </Button>
+    </Link>
+      {/* <LinearGradient
+        // Button Linear Gradient
+        colors={['black', 'rgb(255 0 128)']}
+        style={{height: 100, position: 'absolute', bottom: 0}}
+        >
+        <Text style={{color: 'white', textAlign: 'center', fontSize: 80}}>
+          Login
+          </Text>
+      </LinearGradient> */}
+        { // Campo apenas para testar se o retorno da API funciona corretamente
+          (apiOutput.email) && (
+            <View>
+              <TextInput>{apiOutput.id}</TextInput>
+              <TextInput>{apiOutput.username}</TextInput>
+              <TextInput>{apiOutput.email}</TextInput>
+              <TextInput>{apiOutput.firstName}</TextInput>
+              <TextInput>{apiOutput.lastName}</TextInput>
+              <TextInput>{apiOutput.gender}</TextInput>
+            </View>
+          )
+        }
+    </View>
+  )
+}
--- app/Styles.ts
+++ app/Styles.ts
@@ -17,6 +17,25 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
   },
+
+  textInput: {
+    color: 'white',
+    borderStyle: 'solid',
+    borderColor: 'purple',
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 10,
+    width: '65%',
+    margin: 3
+  },
+
+  logo: { 
+    fontFamily: 'LilitaOne-Regular',
+    fontSize: 65,
+    color: 'rgb(255 0 128)',
+    marginBottom: 50,
+  },
+  
 });
 
 export default styles;
